f02c34f4ac1b04defe5654506b714833fbe65de9
Joe Fehrman <joseph.fehrman@gmail.com>
2019-01-22 18:37:06 -0500

    Created background component.

---
+++ src/app/background/background.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-background',
+  templateUrl: './background.component.html',
+  styleUrls: ['./background.component.scss']
+})
+export class BackgroundComponent implements OnInit {
+  @Input() variant: string = '';
+  variantText: string = '';
+
+  constructor() {}
+
+  ngOnInit() {
+    switch (this.variant) {
+      case 'success':
+        this.variantText = '--success';
+      case 'warning':
+        this.variantText = '--warning';
+      case 'danger':
+        this.variantText = '--danger';
+      case 'attention':
+        this.variantText = '--attention';
+      default:
+        this.variantText = '';
+    }
+  }
+
+}
