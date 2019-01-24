be3d57896c4e6c5d0cf446e91ab68d43aa19bf5f
Joe Fehrman <joseph.fehrman@gmail.com>
2019-01-24 17:52:21 -0500

    Finshed stories for background component and fixed bugs.

--- src/app/background/background.story.ts
+++ src/app/background/background.story.ts
@@ -5,3 +5,27 @@ storiesOf('Background', module)
   .add('Basic background component', () => ({
     component: BackgroundComponent,
   }))
+  .add('Background success', () => ({
+    component: BackgroundComponent,
+    props: {
+      variant: 'success',
+    }
+  }))
+  .add('Background warning', () => ({
+    component: BackgroundComponent,
+    props: {
+      variant: 'warning',
+    }
+  }))
+  .add('Background danger', () => ({
+    component: BackgroundComponent,
+    props: {
+      variant: 'danger',
+    }
+  }))
+  .add('Background attention', () => ({
+    component: BackgroundComponent,
+    props: {
+      variant: 'attention',
+    }
+  }));
